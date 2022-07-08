843ccb962d84ab884e551e8e57df753cba9d95ec
riandroraul <riandroraul@gmail.com>
2022-07-08 14:16:54 +0700

    coba unit test jest

--- src/index.js
+++ src/index.js
@@ -6,4 +6,26 @@ const halo = (nama, umur) => {
     return `halo ${nama} umur saya ${umur}`
 }
 
-module.exports = { halo, sum }
+const calculate = (nilai1, nilai2, opt) => {
+    switch (opt) {
+        case '+':
+            return nilai1 + nilai2;
+            break;
+        case '*':
+        case 'x':
+        case 'X':
+            return nilai1 * nilai2;
+            break;
+        case '-':
+            return nilai1 - nilai2;
+            break;
+        case '/':
+            return nilai1 / nilai2;
+            break;
+        default:
+            return 'operator salah!';
+            break;
+    }
+}
+
+module.exports = { halo, sum, calculate }
--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,16 @@
-const { sum, halo } = require('../src/index')
+const { sum, halo, calculate } = require('../src/index')
 
 test('test sum function', () => {
     const result = sum('3', '5')
-    expect(result).toBe('35')
+    expect(result).toBe('350')
 });
 
 test('test function halo', () => {
     const result = halo('rian', 24)
     expect(result).toBe(24)
+})
+
+test('test function calculate', () => {
+    const result = calculate(2, 3, '+')
+    expect(result).toBe(7)
 })
